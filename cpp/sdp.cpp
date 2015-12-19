f8cd3d1cca5e14518dc07a80e46aae623d4f72d8
Ubuntu <ubuntu@ip-172-31-2-0.us-west-1.compute.internal>
2015-12-19 06:26:09 +0000

    Add cpp implementation

---
+++ cpp/sdp.cpp
@@ -0,0 +1,601 @@
+#include <Eigen/Dense>
+#include <Eigen/Eigenvalues>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <Eigen/Cholesky>
+#include <Eigen/Sparse>
+#include <string>
+#include <vector>
+#define REP(i,j,k) for(int i = j ; i < k ; ++i)
+
+using namespace std;
+
+struct Problem
+{
+    int mdim = 0;
+    int ndim = 0;
+    int nblock = 0;
+    float theta = 1.0;
+    vector<int> block_struct;
+    vector<int> block_base;
+    Eigen::VectorXf c;
+    vector<Eigen::MatrixXf,Eigen::aligned_allocator<Eigen::MatrixXf> > F;
+
+    Eigen::VectorXf xVec;
+    Eigen::MatrixXf xMat;
+    Eigen::MatrixXf yMat;
+};
+
+int parse_input(char* prob_path, Problem & prob) //, int & mdim, int & nblock, int &vector<int> block_struct,)
+{
+
+    ifstream prob_file(prob_path);
+
+    if (prob_file.is_open())
+    {
+
+        string line;
+        int idx = 0;
+        int mdim = 0;
+        int nblock = 0;
+
+
+        vector<int> block_struct;
+        int prev_base = 0;
+        vector<int> block_base;
+        while ( idx <= 2 ) 
+        {
+            getline (prob_file,line);
+            //cout << line << '\n'; 
+            if(line[0] != '"')
+            {
+                replace( line.begin(), line.end(), ',', ' ');
+                replace( line.begin(), line.end(), '{', ' ');
+                replace( line.begin(), line.end(), '}', ' ');
+
+                istringstream iss(line);
+                if (idx == 0)
+                {
+                    iss >> mdim;
+                }
+                else if (idx == 1)
+                {
+                    iss >> nblock;
+                }
+                else if (idx == 2)
+                {
+                    REP(i,0,nblock)
+                    {
+                        int block_size;
+                        iss >> block_size;
+
+                        if (block_size < 0)
+                        {
+                            block_size *= -1;
+                        }
+
+                        block_struct.push_back(block_size);
+                        block_base.push_back(prev_base);
+                        prev_base += block_size;
+                    }
+                }
+                else 
+                {
+                    break;
+                }
+                idx += 1;
+            }
+        }
+
+        //vector<Eigen::MatrixXf> F;
+        Eigen::VectorXf c(mdim);
+        //Eigen::VectorXf c;
+        vector<Eigen::MatrixXf,Eigen::aligned_allocator<Eigen::MatrixXf> > F;
+
+
+        int ndim = 0;
+
+        REP(i,0,nblock)
+        {
+            ndim += block_struct[i];
+        }
+
+        //cout << ndim << endl;
+
+        REP(i,0,mdim+1)
+        {
+            Eigen::MatrixXf mat(ndim,ndim);
+            mat.setZero();
+            F.push_back(mat);
+        }
+
+
+        while ( getline (prob_file,line) ) 
+        { 
+            //cout << line << '\n'; 
+            if(line[0] != '"')
+            {
+                replace( line.begin(), line.end(), ',', ' ');
+                replace( line.begin(), line.end(), '{', ' ');
+                replace( line.begin(), line.end(), '}', ' ');
+
+                istringstream iss(line);
+
+                if( idx == 3)
+                {
+                    REP(i,0,mdim)
+                    {
+                        float val;
+                        iss >> val;
+                        c[i] = val;
+                    }
+
+                }
+                else
+                {
+                    int mat_idx = 0;
+                    int blk_idx = 0;
+                    int i_idx = 0;
+                    int j_idx = 0;
+                    float val = 0;
+
+                    iss >> mat_idx >> blk_idx >> i_idx >> j_idx >> val;
+
+                    int base = block_base[blk_idx-1];
+
+                    int i_abs = i_idx - 1 + base;
+                    int j_abs = j_idx - 1 + base;
+
+                    F[mat_idx](i_abs,j_abs) = val;
+                    F[mat_idx](j_abs,i_abs) = val;
+
+                }
+                idx += 1;
+            }
+        }
+        
+        //cout << c << endl;
+        //REP(i,0, mdim+1)
+        //{
+        //    cout << F[i] << endl;
+        //}
+        prob.F = F;
+        prob.c = c;
+        prob.mdim = mdim;
+        prob.ndim = ndim;
+        prob.nblock = nblock;
+        prob.block_struct = block_struct;
+        prob.block_base = block_base;
+    }
+    {
+        return -1;
+    }
+
+    return 0;
+}
+
+
+int parse_init(char* init_path, Problem & prob)
+{
+    //cout << init_path << endl;
+    ifstream prob_file(init_path);
+
+    if (prob_file.is_open())
+    {
+        string line;
+        int nblock = prob.nblock;
+        int mdim = prob.mdim;
+        int ndim = prob.ndim;
+
+        vector<int> block_struct = prob.block_struct;
+        vector<int> block_base = prob.block_base;
+
+
+        Eigen::VectorXf xVec(mdim);
+        Eigen::MatrixXf xMat(ndim,ndim);
+        Eigen::MatrixXf yMat(ndim,ndim);
+        xVec.setZero();
+        xMat.setZero();
+        yMat.setZero();
+
+        int idx = 0;
+        
+        while ( getline (prob_file,line) ) 
+        {
+
+            replace( line.begin(), line.end(), ',', ' ');
+            replace( line.begin(), line.end(), '{', ' ');
+            replace( line.begin(), line.end(), '}', ' ');
+            istringstream iss(line);
+
+            if (idx == 1)
+            {
+                //parse xVec
+                float val;
+                REP(i, 0, mdim)
+                {
+                    iss >> val;
+                    xVec[i] = val;
+                }
+
+                //cout << xVec << endl;
+            }
+            else if ( idx >=3 && idx <= 2 + nblock )
+            {
+                //parse xMat
+                int blk_nth = idx - 3;
+                float val;
+                int size = block_struct[blk_nth];
+                int base = block_base[blk_nth];
+
+                REP(i, 0, size)
+                {
+                    REP(j, 0, size) 
+                    { 
+                        iss >> val; 
+                        int abs_i = i + base;
+                        int abs_j = j + base;
+                        xMat(abs_i,abs_j) = val;
+                    }
+                }
+            }
+            else if (idx >= 2 + nblock + 2)
+            {
+                //parse yMat
+                int blk_nth = idx - (2+nblock+2);
+                float val;
+                int size = block_struct[blk_nth];
+                int base = block_base[blk_nth];
+
+                REP(i, 0, size)
+                {
+                    REP(j, 0, size) 
+                    { 
+                        iss >> val; 
+                        int abs_i = i + base;
+                        int abs_j = j + base;
+                        yMat(abs_i,abs_j) = val;
+                    }
+                }
+            }
+
+            idx += 1;
+        }
+        prob.xVec=xVec;
+        prob.xMat=xMat;
+        prob.yMat=yMat;
+
+        //cout << xVec << endl;
+        //cout << xMat << endl;
+        //cout << yMat << endl;
+
+    }
+    else
+    {
+        return 1;
+    }
+    
+
+    return 0;
+}
+
+
+
+
+bool is_psd(Eigen::MatrixXf X)
+{
+    Eigen::SelfAdjointEigenSolver<Eigen::MatrixXf> es(X);
+    Eigen::VectorXf eivals = es.eigenvalues();
+    int len = eivals.size();
+
+    REP(i, 0, len)
+    {
+        if (eivals(i) <= 0)
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+Eigen::VectorXf flatten(Eigen::MatrixXf mat)
+{
+    Eigen::MatrixXf temp = mat;
+    temp.transposeInPlace();
+
+    Eigen::VectorXf ret(Eigen::Map<Eigen::VectorXf>(temp.data(), temp.cols()*temp.rows()));
+    return ret;
+}
+
+
+
+Eigen::MatrixXf concatenate( vector<Eigen::MatrixXf,Eigen::aligned_allocator<Eigen::MatrixXf> > & list, int axis = 0 )
+{
+    int n = list.size();
+    int rows = 0;
+    int cols = 0;
+
+    int each_rows = 0;
+    int each_cols = 0;
+    
+    vector<int> dims;
+    if( axis == 0 )
+    {
+        REP(i,0,n)
+        {
+            each_rows = list[i].rows();
+            each_cols = list[i].cols();
+
+            rows += each_rows;
+            dims.push_back(each_rows);
+            cols = each_cols;
+        }
+
+        Eigen::MatrixXf ret(rows, cols);
+        ret.setZero();
+
+        int start_dim = 0;
+        REP(i,0,n) 
+        { 
+            int i0 = start_dim;
+            int j0 = 0;
+            ret.block(i0,j0,dims[i],cols) = list[i];
+            start_dim += dims[i];
+        }
+
+        return ret;
+    }
+    else
+    {
+        REP(i,0,n)
+        {
+            each_rows = list[i].rows();
+            each_cols = list[i].cols();
+
+            rows = each_rows;
+            cols += each_cols;
+            dims.push_back(each_cols);
+        }
+
+        Eigen::MatrixXf ret(rows, cols);
+        ret.setZero();
+
+        int start_dim = 0;
+        REP(i,0,n) 
+        { 
+            int i0 = 0;
+            int j0 = start_dim;
+            ret.block(i0,j0,rows,dims[i]) = list[i];
+            start_dim += dims[i];
+        }
+
+        return ret;
+    }
+}
+
+Eigen::MatrixXf linear_prod( Eigen::VectorXf & y, vector<Eigen::MatrixXf,Eigen::aligned_allocator<Eigen::MatrixXf> > & A)
+{
+    Eigen::MatrixXf ret = A[0];
+    int n = A.size();
+
+    REP(i,1,n)
+    {
+        ret += y(i) * A[i];
+    }
+
+    return ret;
+}
+
+
+// Reference paper: http://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-251j-introduction-to-mathematical-programming-fall-2009/readings/MIT6_251JF09_SDP.pdf
+//
+// Solve the normal equations for D and y:
+// C - theta*X^-1 + theta * X^-1 * D * X^-1 = sum_{i=1}^m y_i A_i
+// A_i*D = 0, i = 1,...,m
+float solve_normal_equation(
+        int mdim,
+        int ndim,
+        Eigen::MatrixXf &X, 
+        vector<Eigen::MatrixXf,Eigen::aligned_allocator<Eigen::MatrixXf> > & A,
+        float theta,
+        Eigen::MatrixXf &C,
+        Eigen::MatrixXf &D,
+        Eigen::VectorXf &y)
+{
+
+
+
+    vector<Eigen::MatrixXf,Eigen::aligned_allocator<Eigen::MatrixXf> > temp;
+    temp.push_back(flatten(X-1/theta*X*C*X));
+
+    Eigen::MatrixXf temp_zeros(mdim,1);
+    temp_zeros.setZero();
+    temp.push_back(temp_zeros);
+    Eigen::MatrixXf N = concatenate(temp);
+    //cout << N << endl;
+    temp.clear();
+
+
+    REP(i,0,mdim)
+    {
+       //cout << "before" << endl;
+       //cout <<  -1/theta *X*A[i]*X << endl;
+       //cout << "after" << endl;
+       //cout <<  flatten(-1/theta *X*A[i]*X) << endl;
+       temp.push_back(flatten(-1/theta *X*A[i]*X));
+    }
+
+
+    Eigen::MatrixXf Q = concatenate(temp,1);
+
+    
+    //cout << Q << endl;
+    temp.clear();
+
+    REP(i,0,mdim)
+    {
+       //cout << "before" << endl;
+       //cout << A[i] << endl;
+       //cout << "after" << endl;
+       //cout << flatten(A[i]).tranpse() << endl;
+       temp.push_back(flatten(A[i]).transpose());
+    }
+
+    //cout << A[mdim-2] << endl;
+    Eigen::MatrixXf P = concatenate(temp);
+    temp.clear();
+
+
+    Eigen::MatrixXf M(ndim*ndim + mdim, ndim*ndim + mdim);
+    M.setZero();
+
+    Eigen::MatrixXf ind(ndim*ndim,ndim*ndim);
+    Eigen::MatrixXf zeros(mdim,mdim);
+    ind.setIdentity();
+    zeros.setZero();
+    M.block(0,0,ndim*ndim,ndim*ndim) = ind;
+    M.block(0,ndim*ndim, ndim*ndim, mdim) = Q;
+    M.block(ndim*ndim,0, mdim, ndim*ndim) = P;
+    M.block(ndim*ndim,ndim*ndim, mdim,mdim) = zeros;
+
+
+    
+    //cout << M << endl;
+    Eigen::VectorXf Z = M.colPivHouseholderQr().solve(N);
+
+    float err = (M*Z-N).norm();
+
+
+
+    D = Z.block(0,0,ndim*ndim,1);
+    y = Z.block(ndim*ndim,0, mdim,1);
+
+
+    D.resize(ndim,ndim);
+    D = 0.5 * (D + D.transpose());
+    
+    //cout << M << endl;
+    //cout << N << endl;
+    //cout << D << endl;
+    //cout << y << endl;
+
+    return err;
+
+}
+
+
+float prod_sum(Eigen::MatrixXf & x, Eigen::MatrixXf &y)
+{
+
+    float ret = 0.0;
+
+    int rows = x.rows();
+    int cols = x.cols();
+
+
+    REP(i,0,rows)
+    {
+        REP(j,0,cols)
+        {
+            ret += x(i,j) * y(i,j);
+        }
+    }
+
+    return ret;
+}
+
+
+void sdp(Problem & prob)
+{
+    int mdim = prob.mdim;
+    int ndim = prob.ndim;
+    int nblock = prob.nblock;
+    float theta = prob.theta;
+    vector<int> & block_struct = prob.block_struct;
+    vector<int> & block_base = prob.block_base;
+
+    Eigen::MatrixXf X = prob.yMat;
+    Eigen::MatrixXf C = -prob.F[0];
+    Eigen::MatrixXf S;
+    Eigen::VectorXf b = -prob.c;
+
+    vector<Eigen::MatrixXf,Eigen::aligned_allocator<Eigen::MatrixXf> > A;
+
+    REP(i,0,mdim)
+    {
+        Eigen::MatrixXf mat = -prob.F[i+1];
+        A.push_back(mat);
+    }
+
+
+    int m = mdim;
+    int n = ndim;
+
+    while (true)
+    {
+
+        float alpha = 0.5;
+        theta = alpha * theta;
+
+        Eigen::VectorXf y;
+        Eigen::MatrixXf D;
+        float err= solve_normal_equation( mdim, ndim, X, A, theta, C, D, y);
+
+        if (err > 1e-2)
+        {
+            break;
+        }
+
+        S = C - linear_prod(y,A);
+
+
+        float t = 1.0;
+
+        while( !is_psd(X + t*D))
+        {
+            t = t * alpha;
+        }
+
+        float primal_criterion = -b.transpose()*y;
+        float dual_criterion = -prod_sum(C,X);
+
+        cout << "primal_criterion: " << primal_criterion << endl;
+        cout << "dual_criterion: " << dual_criterion << endl;
+
+        if (theta < 1e-4)
+        { 
+            break;
+        }
+        else
+        { 
+            X = X + t*D;
+        }
+
+    }
+
+    prob.yMat = X;
+}
+
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        cout << "Usage: sdp problem_file init_file" << endl;
+        exit(1);
+    }
+
+    char * prob_path = argv[1];
+    char * init_path = argv[2];
+
+    Problem prob;
+    cout << "Parsing problem..." << endl;
+    parse_input(prob_path, prob);
+    cout << "Parsing initialization..." << endl;
+    parse_init(init_path, prob);
+
+    cout << "Solving..." << endl;
+    sdp(prob);
+}
--- python/sdp.py
+++ python/sdp.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
+import pprint
 import numpy as np
+import cPickle as pkl
 from scipy.sparse.linalg import arpack
+import sys
+
+pp = pprint.PrettyPrinter(depth=6)
 
 # Reference paper: http://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-251j-introduction-to-mathematical-programming-fall-2009/readings/MIT6_251JF09_SDP.pdf
 
@@ -11,11 +16,15 @@ def solve_normal_equatons(X, A, theta, C):
     # Transfer to MZ=N
     # Solve for Z, where Z:= [D.flatten(), y^T]^T
 
+
+
     m = len(A)
     n = X.shape[0]
 
     N = np.concatenate((((X - X*C*X/theta).flatten()).T, np.mat(np.zeros((m,1)).astype(float))))
+
     Q = np.concatenate([ ((-X*a*X/theta).flatten()).T for a in A], axis = 1)
+
     P = np.concatenate([ a.flatten() for a in A], axis = 0)
     M = np.bmat([[np.mat(np.identity(n*n)), Q],[P, np.mat(np.zeros((m,m)))]])
 
@@ -25,35 +34,18 @@ def solve_normal_equatons(X, A, theta, C):
     residual =  np.linalg.norm(N - M*Z)
 
     D = Z[:n*n, 0].reshape(n,n)
+    D = 0.5 * (D+D.T)
     y = Z[n*n:, 0].reshape(m,1)
+
+
+    #pprint.pprint(A[-2])
+    #pprint.pprint(M)
+    #pprint.pprint(N)
+    #pprint.pprint(Z)
+    #pprint.pprint(D)
+    #pprint.pprint(y)
+
     
-
-    #print [(X*a*X/theta).flatten() for a in A]
-    #print -X*C*X/theta + X
-    #print M
-    #print N
-    #print Z
-    #print D
-    #print y
-    #print M*Z - N
-
-    #print A[0]
-    #print A[1]
-    #print y
-
-    #print 3.75*A[0]-2.25*A[1]
-    #print sum([3.75*A[0],-2.25*A[1]])
-    #print A
-    #print y.T.tolist()[0]
-    #print zip(y.tolist()[0],A)
-
-    #print linear_comb_matrices(y, A)
-    #residual = linear_comb_matrices(y, [X*a*X/theta for a in A]) - X*C*X/theta + X -  D
-    #print residual
-
-    #print D
-    #print 'theta: ' +str(theta)
-
     return {'D':D,'y':y, 'err': residual}
 
 
@@ -66,10 +58,6 @@ def check_residual_norm_equations(X,A,theta,C, D, y):
         residual += prod_sum(a,D)
 
     return residual
-
-    
-
-
 
 
 def constraint_residual(P):
@@ -116,17 +104,6 @@ def rand_pd_mat(n):
 # n: X is nxn matrix
 #
 # return a dictionary containing A, b, C, and a strictly feasible X
-def random_problem(m,n):
-    A = [rand_sym_mat(n) for i in range(m)]
-
-    X = rand_pd_mat(n)
-
-    #X = np.mat(np.diag(np.random.rand(n)))
-
-    b = [prod_sum(X,a) for a in A]
-    C = rand_sym_mat(n)
-
-    return {'A':A, 'X':X, 'b':b, 'C':C, 'theta': 1.0, 'beta':0.25}
 
 def criterion(C,X):
     return prod_sum(C,X)
@@ -188,6 +165,7 @@ def feasible_beta_approx( P , theta = 1.0, beta=0.25):
     while True:
         L = np.linalg.cholesky(X)
         sol = solve_normal_equatons(X,A,theta,C)
+
         if sol['err'] > 1e-2: 
             break
 
@@ -196,6 +174,7 @@ def feasible_beta_approx( P , theta = 1.0, beta=0.25):
         y = np.array(sol['y']).flatten().tolist()
 
         S = C - sum([y_i*A_i for A_i,y_i in zip(A,y)])
+
         stop = np.linalg.norm(L.I*D*(L.T).I)
 
         if stop <= 0.25:
@@ -252,6 +231,7 @@ def sdp(P):
     b = P['b']
     theta = P['theta']
     beta = P['beta']
+    P['hist'] = []
 
     n = X.shape[0]
     m = len(A)
@@ -266,6 +246,7 @@ def sdp(P):
 
        theta = alpha * theta
        sol = solve_normal_equatons(X,A,theta,C)
+
        if sol['err'] > 1e-2: 
            break
 
@@ -281,7 +262,14 @@ def sdp(P):
            t = alpha * t
 
        #print "theta: " + str(theta)
-       print 'criterion: ' + str(prod_sum(C,X))
+       primal_criterion = prod_sum(C,X)
+       dual_criterion = (np.mat(b)*np.mat(y))[0,0]
+       P['hist'].append([primal_criterion, dual_criterion])
+       print 'primal criterion: ' + str(prod_sum(C,X))
+       print 'dual criterion: ' + str((np.mat(b)*np.mat(y))[0,0])
+       print 'duality gap: ' + str(prod_sum(S,X))
+
+
        #print 'constraint: ' +  str(constraint_residual(P))
        #print 'step: ' + str(t)
 
@@ -322,6 +310,149 @@ def test_normal():
 
 
 
+def random_problem(m,n):
+    A = [rand_sym_mat(n) for i in range(m)]
+
+    X = rand_pd_mat(n)
+
+    #X = np.mat(np.diag(np.random.rand(n)))
+
+    b = [prod_sum(X,a) for a in A]
+    C = rand_sym_mat(n)
+
+    return {'A':A, 'X':X, 'b':b, 'C':C, 'theta': 1.0, 'beta':0.25}
+
+def parse_problem_file(path):
+
+    mdim = -1
+    nblocks = -1
+    c = []
+    F = []
+    ndim = -1
+    block_base = []
+
+
+    with open(path, 'r') as f:
+        content = [line.rstrip('\n').strip().split('=')[0] for line in f.readlines()]
+
+        idx = 0
+        for line in content:
+            if not line.startswith('"'):
+                line = line.replace(',',' ')
+                if idx == 0:
+                    mdim = int(line)
+                elif idx ==1:
+                    nblock = int(line)
+                elif idx ==2:
+                    block_struct = [abs(int(x)) for x in line.split()]
+                    ndim = sum(block_struct)
+                    for i in range(mdim+1):
+                        F.append(np.mat(np.zeros((ndim,ndim))))
+
+                    block_base = [sum(block_struct[:i]) for i in range(len(block_struct))]
+                elif idx ==3:
+                    c = [float(x) for x in line.split()]
+                else:
+                    vals = line.split()
+                    n_mat = int(vals[0])
+                    n_block = int(vals[1])
+                    rel_i = int(vals[2])
+                    rel_j = int(vals[3])
+                    val = float(vals[4])
+
+                    abs_i = block_base[n_block-1] + rel_i - 1
+                    abs_j = block_base[n_block-1] + rel_j - 1
+
+                    F[n_mat][abs_i,abs_j] = val
+                    F[n_mat][abs_j,abs_i] = val
+
+                idx += 1
+
+        C = -F[0]
+        b = [-x for x in c]
+        A = [-x for x in F[1:]]
+
+
+        #pp.pprint(C)
+        #pp.pprint(b)
+        #pp.pprint(A)
+        return {'A':A, 'b':b, 'C':C, 'theta': 1.0, 'beta':0.25, 'nblock':nblock, 'block_struct':block_struct, 'block_base':block_base, 'mdim':mdim, 'ndim': ndim}
+        
+
+
+def parse_initial_point(path, P, sparse = True):
+    mdim = P['mdim']
+    ndim = P['ndim']
+    block_struct = P['block_struct']
+    block_base = P['block_base']
+    nblock = P['nblock']
+
+    with open(path, 'r') as f: 
+        content = [line.rstrip('\n').strip().split('=')[0] for line in f.readlines()]
+
+        if sparse:
+            idx = 0
+            yMat = np.mat(np.zeros((ndim,ndim)))
+            xMat = np.mat(np.zeros((ndim,ndim)))
+            mats = [xMat, yMat]
+            for line in content: 
+                line = line.replace(',',' ')
+                if not line.startswith('"'):
+                    if idx == 0:
+                        xVec = [float(x) for x in line.split()]
+                    else:
+                        vals = line.split()
+                        n_mat = int(vals[0])
+                        n_block = int(vals[1])
+                        rel_i = int(vals[2])
+                        rel_j = int(vals[3])
+                        val = float(vals[4])
+
+                        abs_i = block_base[n_block-1] + rel_i - 1
+                        abs_j = block_base[n_block-1] + rel_j - 1
+                        
+                        mats[n_mat-1][abs_i,abs_j] = val
+                        mats[n_mat-1][abs_j,abs_i] = val
+                idx += 1
+            P['X'] = yMat
+        else:
+            xVec_idx = 0
+            yMat = np.mat(np.zeros((ndim,ndim)))
+            while not content[xVec_idx].startswith('xVec'):
+                xVec_idx += 1
+
+            xMat_idx = 0
+            while not content[xMat_idx].startswith('xMat'):
+                xMat_idx += 1
+
+            yMat_idx = 0
+            while not content[yMat_idx].startswith('yMat'):
+                yMat_idx += 1
+
+            
+            #print content[yMat_idx + 1:]
+            #print content[yMat_idx+1:]
+            for blk_idx, blk in enumerate(content[yMat_idx + 1:]):
+                vals = [float(x) for x in blk.split()]
+                blk_size = block_struct[blk_idx]
+                base = block_base[blk_idx]
+
+                #print vals
+                #print block_struct
+                #print blk_idx
+                #print blk_size
+
+                for i in range(blk_size):
+                    for j in range(blk_size):
+                        val = vals[i*blk_size + j]
+
+                        abs_i = base + i
+                        abs_j = base + j
+                        yMat[abs_i,abs_j] = val
+
+
+                P['X'] = yMat
+    return 0
 
 def main():
     #np.random.seed(41392)
@@ -333,12 +464,30 @@ def main():
     P = feasible_beta_approx(P)
 
     sdp(P)
+
     print "end C*X: " + str(prod_sum(P['C'],P['X']))
     print "If feasible: " + str(constraint_residual(P))
 
     return 0
 
+
 if __name__ == "__main__":
-    main()
+
+    if len(sys.argv) != 4:
+        print "Usage: sdp.py problem_file init_file output"
+    else:
+        P = parse_problem_file(sys.argv[1])
+        parse_initial_point(sys.argv[2],P, sparse=False)
+        #P = feasible_beta_approx(P)
+        sdp(P)
+        print "end C*X: " + str(prod_sum(P['C'],P['X']))
+        print "If feasible: " + str(constraint_residual(P))
+
+        with open(sys.argv[3], 'w') as f:
+            pkl.dump(P,f)
+
+
+
+    #main()
     #test_normal()
 
